bddb78191586e23373888038578d3d1e4911afd7
jdeath <17914369+jdeath@users.noreply.github.com>
2023-12-27 19:41:16 -0500

    Add files via upload

---
+++ custom_components/ble_yc01/BLE_YC01/__init__.py
@@ -0,0 +1,8 @@
+"""Parser for RD200 BLE advertisements."""
+from __future__ import annotations
+
+from .parser import YC01BluetoothDeviceData, YC01Device
+
+__version__ = "0.5.3"
+
+__all__ = ["YC01BluetoothDeviceData", "YC01Device"]
---
+++ custom_components/ble_yc01/sensor.py
@@ -0,0 +1,172 @@
+"""Support for YC01 ble sensors."""
+from __future__ import annotations
+
+import logging
+
+from .BLE_YC01 import YC01Device
+
+from homeassistant import config_entries
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorEntityDescription,
+    SensorStateClass,
+)
+from homeassistant.const import (
+    CONCENTRATION_PARTS_PER_BILLION,
+    CONCENTRATION_PARTS_PER_MILLION,
+    LIGHT_LUX,
+    PERCENTAGE,
+    UnitOfPressure,
+    UnitOfTemperature,
+    UnitOfTime,
+    UnitOfElectricPotential,
+    CONDUCTIVITY,
+)
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.device_registry import CONNECTION_BLUETOOTH
+from homeassistant.helpers.entity import DeviceInfo, EntityCategory
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import StateType
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+)
+from homeassistant.util.unit_system import METRIC_SYSTEM
+
+from .const import DOMAIN, VOLUME_BECQUEREL, VOLUME_PICOCURIE
+
+_LOGGER = logging.getLogger(__name__)
+
+SENSORS_MAPPING_TEMPLATE: dict[str, SensorEntityDescription] = {
+    "EC": SensorEntityDescription(
+        key="EC",
+        name="Electrical Conductivity",
+        native_unit_of_measurement=CONDUCTIVITY,
+        state_class=SensorStateClass.MEASUREMENT,
+        icon="mdi:flash-triangle-outline",
+    ),
+    "salt": SensorEntityDescription(
+        key="salt",
+        name="Salt",
+        native_unit_of_measurement=CONCENTRATION_PARTS_PER_MILLION,
+        state_class=SensorStateClass.MEASUREMENT,
+        icon="mdi:shaker-outline",
+    ),
+    "ORP": SensorEntityDescription(
+        key="ORP",
+        name="Oxidation-reduction potential",
+        native_unit_of_measurement=UnitOfElectricPotential.VOLT,
+        state_class=SensorStateClass.MEASUREMENT,
+        device_class=SensorDeviceClass.VOLTAGE,
+        icon="mdi:alpha-v-circle",
+    ),
+    "TDS": SensorEntityDescription(
+        key="TDS",
+        name="Total Dissolved Solids",
+        native_unit_of_measurement=CONCENTRATION_PARTS_PER_MILLION,
+        state_class=SensorStateClass.MEASUREMENT,
+        icon="mdi:dots-grid",
+    ),
+    "pH": SensorEntityDescription(
+        key="pH",
+        name="pH",
+        device_class=SensorDeviceClass.PH,
+        state_class=SensorStateClass.MEASUREMENT,
+        icon="mdi:ph",
+    ),
+    "battery": SensorEntityDescription(
+        key="battery",
+        name="Battery",
+        state_class=SensorStateClass.MEASUREMENT,
+        device_class=SensorDeviceClass.BATTERY,
+        native_unit_of_measurement=PERCENTAGE,
+        icon="mdi:battery",
+    ),
+    "cloro": SensorEntityDescription(
+        key="cloro",
+        name="Free Chlorine",
+        state_class=SensorStateClass.MEASUREMENT,
+        native_unit_of_measurement=CONCENTRATION_PARTS_PER_MILLION,
+        icon="mdi:chemical-weapon",
+    ),
+    "temperature": SensorEntityDescription(
+        key="temperature",
+        name="Temperature",
+        state_class=SensorStateClass.MEASUREMENT,
+        device_class=SensorDeviceClass.TEMPERATURE,
+        native_unit_of_measurement=UnitOfTemperature.CELSIUS,
+        icon="mdi:pool-thermometer",
+    ),
+}
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: config_entries.ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up the YC01 BLE sensors."""
+    is_metric = hass.config.units is METRIC_SYSTEM
+
+    coordinator: DataUpdateCoordinator[YC01Device] = hass.data[DOMAIN][entry.entry_id]
+    sensors_mapping = SENSORS_MAPPING_TEMPLATE.copy()
+    entities = []
+    _LOGGER.debug("got sensors: %s", coordinator.data.sensors)
+    for sensor_type, sensor_value in coordinator.data.sensors.items():
+        if sensor_type not in sensors_mapping:
+            _LOGGER.debug(
+                "Unknown sensor type detected: %s, %s",
+                sensor_type,
+                sensor_value,
+            )
+            continue
+        entities.append(
+            YC01Sensor(coordinator, coordinator.data, sensors_mapping[sensor_type])
+        )
+
+    async_add_entities(entities)
+
+
+class YC01Sensor(CoordinatorEntity[DataUpdateCoordinator[YC01Device]], SensorEntity):
+    """YC01 BLE sensors for the device."""
+
+    #_attr_state_class = SensorStateClass.MEASUREMENT
+    _attr_has_entity_name = True
+
+    def __init__(
+        self,
+        coordinator: DataUpdateCoordinator,
+        YC01_device: YC01Device,
+        entity_description: SensorEntityDescription,
+    ) -> None:
+        """Populate the YC01 entity with relevant data."""
+        super().__init__(coordinator)
+        self.entity_description = entity_description
+
+        name = f"{YC01_device.name} {YC01_device.identifier}"
+
+        self._attr_unique_id = f"{name}_{entity_description.key}"
+
+        self._id = YC01_device.address
+        self._attr_device_info = DeviceInfo(
+            connections={
+                (
+                    CONNECTION_BLUETOOTH,
+                    YC01_device.address,
+                )
+            },
+            name=name,
+            manufacturer="YC01",
+            model="YC01",
+            hw_version=YC01_device.hw_version,
+            sw_version=YC01_device.sw_version,
+        )
+
+    @property
+    def native_value(self) -> StateType:
+        """Return the value reported by the sensor."""
+        try:
+            return self.coordinator.data.sensors[self.entity_description.key]
+        except KeyError:
+            return None
